c5ccc6a7f42c202d10c696a3635cab2b560352a3
MgAdamczyk <mg.adamczyk96@gmail.com>
2020-03-13 20:52:40 +0100

    Added file

---
+++ number-guessing/numberGuessing.py
@@ -0,0 +1,24 @@
+###
+# Program ma za zadanie wylosować liczbę,
+# a użytkownik ma ją zgadnąć.
+###
+
+import random
+
+print("Program losuje liczbę z zakresu 1-100...")
+x = random.randint(1,100)
+
+#Podgląd wylosowanej liczby
+# print(x)
+
+print("A teraz zgadij wylosowaną liczbę :)")
+
+while True:
+    a = int(input("Wpisz swoją liczbę: "))
+    if x == a:
+        print("Zgadłeś wylosowaną liczbę, która wynosi: " + str(x))
+        break
+    elif x > a:
+        print("Zbyt mała liczba")
+    else:
+        print("Zbyt duża liczba")
