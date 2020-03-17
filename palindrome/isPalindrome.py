210bce26c94862a1f547f44a8da2d245dbb749e3
MgAdamczyk <mg.adamczyk96@gmail.com>
2020-03-17 19:27:57 +0100

    Added file

---
+++ palindrome/isPalindrome.py
@@ -0,0 +1,24 @@
+###
+# Program sprawdza, czy podane przez
+# użytkownika słowo jest palindromem.
+###
+
+txt = input("Podaj słowo: ")
+txt=txt.upper()
+
+i=0
+j=len(txt)
+a = True
+
+while i < j:
+    if txt[i]== txt[j-1]:
+        i+=1
+        j-=1
+    else:
+        a = False
+        break
+
+if a == True:
+    print("Podane słowo: \'" + txt + "\' jest palindromem")
+else:
+    print("Podane słowo: \'" + txt + "\' nie jest palindromem")
